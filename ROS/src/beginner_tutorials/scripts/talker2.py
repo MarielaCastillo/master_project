50f06c02c30f52ce36a3aef5602202e4d3a2f140
Mariela <mariela.castillo.mc2@gmail.com>
2022-07-04 13:40:35 +0200

    Changes to talker2.py

--- ROS/src/beginner_tutorials/scripts/talker2.py
+++ ROS/src/beginner_tutorials/scripts/talker2.py
@@ -38,80 +38,96 @@
 
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
 import os
 import numpy as np
 import pandas as pd
 
-def read_text_file(file_path):
-    print("file_path:", file_path)
-    print(type(file_path))
-    df = pd.read_csv(file_path, sep=" ", header=None)
-    
+from beginner_tutorials.msg import MyArray
 
+
+def read_text_file(file_path):
+    df = pd.read_csv(file_path, sep=" ", header=None)
     df = df.drop(df.columns[[3]], axis=1)
-    df.columns.name = None
-    df.reset_index(drop=True, inplace=True)
+
     return df
 
-def read_text_file2(file_path): ##Correct but takes time
-    #values_pointcloud = []
-    values_pointcloud = np.zeros((1,3))
-    with open(file_path, 'r') as f:
-        print(file_path)
-        for line in f: 
-            values = line.split(' ')
-            #values_pointcloud = np.r_[values_pointcloud,[[7,8,9]]]
-            values_pointcloud = np.append(values_pointcloud, [[float(values[0]), float(values[1]), float(values[2])]], axis = 0)
-
-    print("values", values)
-    print("values_pointcloud", values_pointcloud)
-    print(len(values_pointcloud))
-    print(len(values_pointcloud[0]))
-    print(len(values_pointcloud[1]))
-    print(len(values_pointcloud[2])) 
-
-    #file = f.read()
-    #values = file.split(' ')
-    #return float(values[0]), float(values[1]), float(values[2])
-    return values_pointcloud
-
-def read_text_file1(file_path): ##just first line
-    with open(file_path, 'r') as f:
-        print(file_path)
-        file = f.read()
-        values = file.split(' ')
-        return float(values[0]), float(values[1]), float(values[2])
-
-def talker(point_cloud):
-    pub = rospy.Publisher('chatter2', String, queue_size=10)
-    rospy.init_node('talker2', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    
-    #hello_str = "hello world %s" % rospy.get_time()
-    hello_str = "hello world %s" % point_cloud
-
-    rospy.loginfo(hello_str)
-    pub.publish(hello_str)
-    rate.sleep()
 
 if __name__ == '__main__':
     try:
-        #while not rospy.is_shutdown():
-            folder_path = '/home/miw/Documents/MasterProject/KITTI/pointcloud'
+            
+            rospy.init_node('talker2', anonymous=True)
+            pub = rospy.Publisher('chatter2', MyArray, queue_size=10)
+            rate = rospy.Rate(10) # 10hz
 
+        #while not rospy.is_shutdown():
+
+            folder_path = '/home/miw/Documents/MasterProject/KITTI/pointcloud'
+            
+            #my_array_for_publishing = Float64MultiArray()
+            my_array_for_publishing = MyArray()
+            print("layout ", my_array_for_publishing.layout)
+            
 
                 # iterate through all file
             for file in sorted(os.listdir(folder_path)):
-                print("asdf file", file)
             # Check whether file is in text format or not
                 if file.endswith(".txt"):
-                    print("asdf4")
                     file_path = f"{folder_path}/{file}"
-
+                    print()
+                    print("asdf1")
                     point_cloud = read_text_file(file_path)
 
-                    talker(point_cloud)
+                    hello_str = "hello world %s" % point_cloud
+
+                    pcArray = point_cloud.to_numpy()
+                    #print(len(pcArray))
+                    #print(pcArray.shape)
+                    #my_array_for_publishing.layout.dim = [len(pcArray),3]
+                    #print("asdfpcArray",type(pcArray))
+                    
+                    
+                    
+                    #my_msg = Float64MultiArray() 
+                    #my_msg.data = point_cloud
                     print()
+                    #print("asdf2")
+                    #print()
+                    #print("asdf3",type(my_msg))
+                    #print("asdf4",type(my_msg.data))
+                    print()
+                    #print("asdf5")
+                    #rospy.loginfo(my_msg.data)
+
+                    #print("test", pcArray)
+
+                    #my_array_for_publishing = Float64MultiArray(data=pcArray)
+
+                    
+                    my_array_for_publishing.data = pcArray
+                    #print("my_array_for_publishing ", my_array_for_publishing)
+
+                    #print("asdfpcArray",type(my_array_for_publishing))
+
+                    #pub.publish(pcArray)
+
+
+
+
+                    my_array_for_publishing.layout.dim = [1]
+                    my_array_for_publishing.data = [24]
+                    print("my_array_for_publishing ", my_array_for_publishing)
+
+                    print("asdfpcArray",type(my_array_for_publishing))
+
+                    pub.publish(my_array_for_publishing)
+                    print()
+                    print("asdf6")
+                    rate.sleep()
+
+                    ## cambiar a Float64MultiArray
+                    
+            print()
 
     except rospy.ROSInterruptException:
         pass
