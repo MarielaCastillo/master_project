1e58eb4c9d3d73dcbcb1c86b2feefb51feba40a2
Mariela <mariela.castillo.mc2@gmail.com>
2022-07-17 20:01:49 +0200

    Code cleanup for full model

--- master_project/efnetLMmoduleSS.py
+++ master_project/efnetLMmoduleSS.py
@@ -1,65 +1,28 @@
 from tabnanny import check
 import torch
-import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-import torchvision.transforms as transforms
-from efficientnet_pytorch import EfficientNet
-from torch.utils.data import Dataset
-from collections import OrderedDict
 from efnetLMmoduleExpert1SS import MultiModalDataset
 
 import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.unet.decoder import UnetDecoder
 from segmentation_models_pytorch.base.heads import SegmentationHead
 
-import pandas as pd
-import os
-from skimage import io
-
-import numpy as np
-from torch.utils.data.dataloader import default_collate
-
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-#fl_ir_aligned_1570722156_952177040
-#fl_rgb_1570722156_952177040
-#fl_rgb_labels_1570722156_952177040
-
-#1570722156_952177040
-#1570722156_952177040
-#1570722156_952177040
-
-#fl_ir_aligned_1570729614_4316866600
-#fl_rgb_1570729614_4316866600
-
-#1570729614_4316866600
-#1570729614_4316866600
-
 ####Model
 class LitModelEfficientNetFull(pl.LightningModule):
-    #https://github.com/gthparch/edgeBench/blob/master/pytorch/models/cifarnet.py#L23
-    #def __init__(self, batch_size, transform, model1, model2):
     def __init__(self, batch_size, transform, model1, model2):
         super(LitModelEfficientNetFull, self).__init__()
         self.batch_size = batch_size
         self.transform = transform
 
         self.criterion = nn.CrossEntropyLoss()
-        #self.criterion = nn.MSELoss()
 
         self.cnnexpertRGB = model1
-        #self.cnnexpertRGB = UNetExpert1(inchannels=3, numclasses=3)
-        #self.cnnexpertDepth = model2
-        #self.cnnexpertRGB = CNNExpert1(3, 3) ### model_weights[key.replace("auto_encoder.", "")] = model_weights.pop(key)
         self.cnnexpertDepth = model2
-        #self.cnnexpertLidar = CNNExpert1(2, 3)
-        #self.cnnexpertThermo = CNNExpert1(1, 3)
-        #hidden_concat_dim = self.cnnexpertRGB.hidden_dim + self.cnnexpertDepth.hidden_dim + self.cnnexpertLidar.hidden_dim + self.cnnexpertThermo.hidden_dim
-        
-        #hidden_concat_dim = self.cnnexpertDepth.hidden_dim + self.cnnexpertLidar.hidden_dim + self.cnnexpertThermo.hidden_dim ## definir este numero con base en el encoder
 
         encoder_channels = list(self.cnnexpertRGB.model.encoder.out_channels)
         encoder_channels = [ 2 * a  for a in encoder_channels]
@@ -76,22 +39,13 @@ class LitModelEfficientNetFull(pl.LightningModule):
                 in_channels=decoder_channels[-1],
                 out_channels=2,
                 kernel_size=3,
+                activation='softmax'
             )
 
     def forward(self, x1, x2=None, x3=None, x4=None):
-    #def forward(self, hidden_statergb, outrgb, hidden_statedepth, outdepth, x3, x4): ### here1 ## 1 input (x) con los dict
-
         hidden_states_rgb, outrgb = self.cnnexpertRGB(x1)
-        hidden_states_depth, outdepth = self.cnnexpertRGB(x2) #### cambiar a x2
-        #hidden_stateLidar, outLidar = self.cnnexpertLidar(x3)
-        #hidden_stateThermo, outThermo = self.cnnexpertThermo(x4)
+        hidden_states_depth, outdepth = self.cnnexpertRGB(x2)
 
-        #hidden_statergb = torch.as_tensor(hidden_statergb)
-
-
-
-        ## coomo es el tamanio del encoder
-        # outconcatexpertclassifier = torch.stack([outrgb, outdepth], dim = -1) #(matrix de wxh) #stack las imaagenes
         input_gating = []
         for hidden_state_rgb, hidden_state_depth in zip(hidden_states_rgb, hidden_states_depth):
             input_gating.append(torch.cat([hidden_state_rgb, hidden_state_depth], dim = 1))
@@ -104,21 +58,9 @@ class LitModelEfficientNetFull(pl.LightningModule):
         # 2. el combinado
         outfinal = gating[:, 0] * outrgb + gating[:, 1] * outdepth
 
-
-
-        ### en vez de sumar, agarrar el max(max probability, o max energy)
-        return outfinal ######## Regression instead of  classif. Check dim
-
+        return outfinal
 
     def train_dataloader(self):
-        #trainset = torchvision.datasets.Kitti(root='./data', train=True, download=True, transform=self.transform)
-        #trainset = ThermalDataset("~/Downloads/00")
-
-        
-        
-        # trainset = MultiModalDataset(csv_file=dir_path + '/' + 'multimodal.csv', root_dir=dir_path + '/'+'multimodalfolder', transform=self.transform)
-        #trainset = MultiModalDataset(csv_file=dir_path + '/' + 'multimodal.csv', root_dir=dir_path + '/'+'multimodalfolder', transform=self.transform)
-
         print("dir_path ", dir_path)
         print(dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00/fl_rgb/')
         dir_path2 = dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00'
@@ -128,30 +70,21 @@ class LitModelEfficientNetFull(pl.LightningModule):
                                         label_path= dir_path2 + '/' + 'fl_rgb_labels',
                                         transform= self.transform)       
 
-        #print("dir_path ", dir_path)
-        #trainset = MultiModalDataset(csv_file='multimodal.csv', root_dir='multimodalfolder', transform=self.transform)
-        
-
-
-
-        #trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-        #                                   download=True, transform=self.transform)
-        
-
-
-
-        #trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size,
-        #                                    shuffle=True, num_workers=10, collate_fn = collate_multimodal) #2
-
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size,
                                             shuffle=True, num_workers=10) #2
         return trainloader
 
     def test_dataloader(self):
-        #testset = torchvision.datasets.Kitti(root='./data', train=False,
-        #                                      download=True, transform=self.transform)
-        testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                         download=True, transform=self.transform)
+        print("dir_path ", dir_path)
+        print(dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00/fl_rgb/')
+        dir_path2 = dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00'
+
+        testset = MultiModalDataset(rgb_path= dir_path2 + '/' + 'fl_rgb', 
+                                        thermo_path = dir_path2 + '/' + 'fl_ir_aligned',
+                                        label_path= dir_path2 + '/' + 'fl_rgb_labels',
+                                        transform= self.transform)       
+
+
 
 
         #test setdatoenvivo = # no en train, pero en vivo.  1 rgb 1bw 1 point cloud
@@ -191,49 +124,10 @@ class LitModelEfficientNetFull(pl.LightningModule):
         #inputrgb, inputdepth, labels = train_batch
         inputrgb, labels = train_batch
         labels = labels.long()
-        #inputs = train_batch
-        #optimizer.zero_grad()
-        batch_size = 8
-        w, h = 128, 128
-        #w=1392
-        #h = 512
 
-        #labels = torch.rand(batch_size, 3)
-        #inputrgb = torch.rand(batch_size, 3, w, h)
-        inputdepth = torch.rand(batch_size, 1, w, h)
-        inputLidar = torch.rand(batch_size, 2, w, h)
-        inputThermo = torch.rand(batch_size, 1, w, h)
-
-
-        #print("lidar shape: ", inputLidar.shape)
-        #print("rgb shape: ", inputrgb.shape)
-        #print("depth shape: ", inputdepth.shape)
-        #print(type(inputLidar))
-        #print(type(inputrgb))
-
-        ### add 1 channel to depth
-        #inputdepth = np.expand_dims(inputdepth, axis=1)
-        #print("depth shape2: ", inputdepth.shape)
-
-        ## reorder rgb shape 111, 512, 1392, 3 to have 111, 3, 512, 1392
-        #inputrgb = inputrgb.permute(0, 3, 1, 2)
-        #print("rgb shape2: ", inputrgb.shape)
-
-        
-        #inputrgb = torch.rand(batch_size, 3, w, h)
-
-        #outputs = self(inputs['rgb'], inputs['depth'], inputLidar, inputThermo) #forward(x1, x2, x3, x4)
-        #loss = self.criterion(outputs, inputs['label'])
-
-        outputs = self(inputrgb, inputrgb) #forward(x1, x2, x3, x4)
-        #outputs = self(hidden_statergb, outrgb, hidden_statedepth, outdepth, inputLidar, inputThermo) #forward(x1, x2, x3, x4)
+        outputs = self(inputrgb, inputrgb)  # forward(x1, x2, x3, x4) ### CAMBIAR
         loss = self.criterion(outputs, labels)
         return loss
-
-        
-        #histate, outputs = self(inputrgb) #forward(x1, x2, x3, x4)
-        #loss = self.criterion(outputs, labels)
-        #return loss
 
     def test_step(self, test_batch, batch_idx):
         images, labels = test_batch
@@ -242,9 +136,5 @@ class LitModelEfficientNetFull(pl.LightningModule):
         _, predicted = torch.max(outputs.data, 1)
 
         loss = F.cross_entropy(predicted, labels)
-        #loss = F.mse_loss(predicted, labels)
 
         return loss
-
-##Revisar
-#torch.save(model.state_dict(), save_path)
--- master_project/efnetmainSS.py
+++ master_project/efnetmainSS.py
@@ -1,15 +1,9 @@
 from pytorch_lightning import Trainer
-#from efnetLMmodule import LitModelEfficientNet
 from efnetLMmoduleExpert1SS import LitModelEfficientNet
 from efnetLMmoduleSS import LitModelEfficientNetFull
 import torchvision.transforms as transforms
 
-from efficientnet_pytorch import EfficientNet
-import torch.nn as nn
-
-import torch
-
-import segmentation_models_pytorch as smp
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -19,38 +13,21 @@ def main():
         [transforms.ToTensor(),
         transforms.Resize((320, 960)),
         transforms.Normalize((0.5,), (0.5,))])
-    model1 = LitModelEfficientNet.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints/epoch=0-step=489.ckpt", transform=transform)
-    model2 = LitModelEfficientNet.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints/epoch=1-step=978.ckpt", transform=transform)
-    expert_rgb = model1.cnnexpertRGB
-    expert_thermo = model2.cnnexpertRGB
-    '''
-    ### Expert2 #############################################################################
-    model2 = CNNExpert1(1,3)
-    checkpoint = torch.load("best_modelExp2.ckpt")
+    model_rgb = LitModelEfficientNet.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints/epoch=0-step=489.ckpt", transform=transform)
+    model_thermo = LitModelEfficientNet.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints/epoch=1-step=978.ckpt", transform=transform)
+    expert_rgb = model_rgb.cnnexpertRGB
+    expert_thermo = model_thermo.cnnexpertRGB
 
-    model_weights = checkpoint["state_dict"]
-
-    # update keys by dropping `auto_encoder.`
-    for key in list(model_weights):
-        model_weights[key.replace("cnnexpertDepth.", "")] = model_weights.pop(key)
-        
-    model2.load_state_dict(model_weights)
-    '''
-
-
-
-    #model = LitModelEfficientNet(8, transform) ##this number is batch size 
-    #model = LitModelEfficientNet(8, transform, model1, model2) ##this number is batch size 
+    checkpoint_callback = ModelCheckpoint(dirpath='models_with_pretrained_experts/')
+    trainer = Trainer(accelerator="cpu",max_epochs=2, callbacks=[checkpoint_callback])
     model = LitModelEfficientNetFull(1, transform, expert_rgb, expert_thermo) 
+    trainer.fit(model)
     
-    #check how to fix when total is not divisible by number of elements...
-    #drop_batch
 
     #trainer = Trainer(gpus=1, max_epochs=2)
-    print(dir_path)
     #trainer = Trainer(accelerator="cpu",max_epochs=2, default_root_dir=dir_path+ '/' + 'models')
-    trainer = Trainer(accelerator="cpu",max_epochs=2)
-    trainer.fit(model)
+
+    
     #trainer.save_checkpoint("best_model.ckpt")
 
 
