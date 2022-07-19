07e776f1924f70d754f35c39bbfaf7608f9df372
Mariela <mariela.castillo.mc2@gmail.com>
2022-07-19 13:15:40 +0200

    Reorganising files

--- efnetLMmoduleSS.py
+++ efnetLMmoduleSS.py
@@ -1,18 +1,14 @@
-from tabnanny import check
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import pytorch_lightning as pl
 from efnetLMmoduleExpert1SS import MultiModalDataset
 
-import segmentation_models_pytorch as smp
 from segmentation_models_pytorch.unet.decoder import UnetDecoder
 from segmentation_models_pytorch.base.heads import SegmentationHead
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-####Model
 class LitModelEfficientNetFull(pl.LightningModule):
     def __init__(self, batch_size, transform_rgb, transform_thermo, model1, model2):
         super(LitModelEfficientNetFull, self).__init__()
@@ -52,18 +48,11 @@ class LitModelEfficientNetFull(pl.LightningModule):
             input_gating.append(torch.cat([hidden_state_rgb, hidden_state_depth], dim = 1))
         gating = self.decoder(*input_gating)
         gating = self.head(gating)
-
-        # 2 sopas
-        # 1. regresar el que tenga maas prob
-
-        # 2. el combinado
         outfinal = gating[:, 0] * outrgb + gating[:, 1] * outdepth
 
-        return outfinal  # here breakpoint for visualizer
+        return outfinal
 
     def train_dataloader(self):
-        print("dir_path ", dir_path)
-        print(dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00/fl_rgb/')
         dir_path2 = dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00'
 
         trainset = MultiModalDataset(rgb_path= dir_path2 + '/' + 'fl_rgb', 
@@ -76,8 +65,6 @@ class LitModelEfficientNetFull(pl.LightningModule):
         return trainloader
 
     def test_dataloader(self):
-        print("dir_path ", dir_path)
-        print(dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00/fl_rgb/')
         dir_path2 = dir_path + '/' + 'thermaldatasetfolder/train/seq_00_day/00'
 
         testset = MultiModalDataset(rgb_path= dir_path2 + '/' + 'fl_rgb', 
@@ -85,58 +72,32 @@ class LitModelEfficientNetFull(pl.LightningModule):
                                         label_path= dir_path2 + '/' + 'fl_rgb_labels',
                                         transform_rgb= self.transform_rgb, transform_thermo=self.transform_thermo)       
 
-
-
-
-        #test setdatoenvivo = # no en train, pero en vivo.  1 rgb 1bw 1 point cloud
         testloader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size,
-                                            shuffle=False, num_workers=10) #2
+                                            shuffle=False, num_workers=10)
         return testloader
 
     ### optimizers and schedulers
     def configure_optimizers(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
         return optimizer
-    '''
-    def training_step_before(self, train_batch, batch_idx): #### CUDA
-        inputs, labels = train_batch
-        #optimizer.zero_grad()
-        batch_size = 8
-        w, h = 128, 128
-        #w=1392
-        #h = 512
-        #labels = torch.randint(0, 2, [batch_size]).cuda() #high exclusive low inclusive ###### aquii posiciones con funcioon GPS -> posvector
-        #inputrgb = torch.rand(batch_size, 3, w, h).cuda()
-        #inputdepth = torch.rand(batch_size, 1, w, h).cuda()
-        #inputLidar = torch.rand(batch_size, 2, w, h).cuda()
-        #inputThermo = torch.rand(batch_size, 1, w, h).cuda()
-
-        labels = torch.rand(batch_size, 3)
-        inputrgb = torch.rand(batch_size, 3, w, h)
-        inputdepth = torch.rand(batch_size, 1, w, h)
-        inputLidar = torch.rand(batch_size, 2, w, h)
-        inputThermo = torch.rand(batch_size, 1, w, h)
-        outputs = self(inputrgb, inputdepth, inputLidar, inputThermo) #forward(x1, x2, x3, x4)
-        loss = self.criterion(outputs, labels)
-        return loss
-    '''
     
-    def training_step(self, train_batch, batch_idx):
-        #inputrgb, inputdepth, labels = train_batch
+    def training_step(self, train_batch):
         input_rgb, input_thermo, labels = train_batch
         labels = labels.long()
 
-        outputs = self(input_rgb, input_thermo)  # forward(x1, x2, x3, x4) ### CAMBIAR
+        outputs = self(input_rgb, input_thermo)
         loss = self.criterion(outputs, labels)
         return loss
 
-    def test_step(self, test_batch, batch_idx):
+    def test_step(self, test_batch):
         # check this
-        images, labels = test_batch
-        outputs = self(images)
-    
-        _, predicted = torch.max(outputs.data, 1)
+        input_rgb, input_thermo, labels = test_batch
+        labels = labels.long()
 
-        loss = F.cross_entropy(predicted, labels)
+        outputs = self(input_rgb, input_thermo)
+        loss = self.criterion(outputs, labels)
+
+        #_, predicted = torch.max(outputs.data, 1)
+        #loss = F.cross_entropy(predicted, labels)
 
         return loss
