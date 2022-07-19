55d6c9e1281816c5d06561d17f916f38c96c5ae7
Mariela <mariela.castillo.mc2@gmail.com>
2022-07-19 13:52:23 +0200

    Add eval files

---
+++ eval_rgb.py
@@ -0,0 +1,19 @@
+from pytorch_lightning import Trainer
+import torchvision.transforms as transforms
+from expert_rgb_module import LitModelEfficientNetRgb
+
+def main():
+    transform_rgb = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((320, 960)),
+        transforms.Normalize((0.5,), (0.5,))])
+
+    model = LitModelEfficientNetRgb.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints_rgb/epoch=0-step=489.ckpt", transform=transform_rgb)
+    model.eval()
+    trainer = Trainer(accelerator="cpu",max_epochs=2)
+    trainer.test(model=model)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ eval_thermo.py
@@ -0,0 +1,24 @@
+import torch
+from pytorch_lightning import Trainer
+import torchvision.transforms as transforms
+
+from expert_thermo_module import LitModelEfficientNetThermo
+from expert_thermo_module import ScaleThermal
+
+
+def main():
+    transform_thermo = transforms.Compose(
+        [torch.tensor,
+        ScaleThermal(max_value=30000),
+        transforms.Resize((320, 960)),
+        transforms.Normalize((0.5,), (0.5,))])
+
+    model = LitModelEfficientNetThermo.load_from_checkpoint(batch_size=1, checkpoint_path="checkpoints_thermo/epoch=1-step=978.ckpt", transform=transform_thermo)
+    model.eval()
+    trainer = Trainer(accelerator="cpu",max_epochs=2)
+    trainer.test(model=model)
+
+
+
+if __name__ == "__main__":
+    main()
