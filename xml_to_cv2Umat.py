4c0223de8e538298154dea0e335869665411ec48
Mariela <mariela.castillo.mc2@gmail.com>
2022-07-26 12:00:07 +0200

    Clean xml to image

--- xml_to_cv2Umat.py
+++ xml_to_cv2Umat.py
@@ -1,38 +1,12 @@
 import os
 import numpy as np
 import pandas as pd
-import itertools as it
 import PIL.Image
 import torch
 import xml.etree.ElementTree as ET 
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-def main2():
-    # define folders and file
-    dir_path3 = dir_path + '/' + 'align'
-    txt_file = dir_path3 + '/' + 'align_train.txt'
-    label_path=dir_path3 + '/' + 'Annotations'
-
-    file_names = pd.read_csv(txt_file, sep=" ", header=None)
-    print(type(file_names))
-    print("name of file",file_names.loc[0, 0])
-
-    lbl_str = ''.join(label_path)
-
-    file_name = file_names.loc[0, 0].replace(" ", "")
-
-    xml_path = os.path.join(lbl_str, file_name+".xml")
-    print("xml_path", xml_path)
-    xml_file = pd.read_xml(xml_path)
-
-
-    #for index, row in file_names.iterrows():
-    #    print("index: ", index)
-    #    print("row: ", row)
-    print(file_names.shape)
-    #print(file_names)
 
 def tensor_to_image(tensor):
     tensor = tensor*255
@@ -44,19 +18,11 @@ def tensor_to_image(tensor):
 
 
 def get_tensor_painted(root, tensor, file):
-    # print(root)
-    # print(root[5][0].text)
+
     length = len(root.findall('object'))
     
     # print("number of objects: ", length)
     for i in range(5, 5+length):
-        '''
-        print(root[i][0].text)
-        print(root[i][2][0].text)
-        print(root[i][2][1].text)
-        print(root[i][2][2].text)
-        print(root[i][2][3].text)
-        '''
         class_name = root[i][0].text
         xmin = int(root[i][2][0].text)
         ymin = int(root[i][2][1].text)
@@ -86,37 +52,6 @@ def get_tensor_painted(root, tensor, file):
     
     return tensor
 
-def just_first():
-    # define folders and file
-    dir_path3 = dir_path + '/' + 'align'
-    txt_file = dir_path3 + '/' + 'align_train.txt'
-    label_path=dir_path3 + '/' + 'Annotations'
-
-    # get path of the xml file
-    file_names = pd.read_csv(txt_file, sep=" ", header=None)
-    lbl_str = ''.join(label_path) #change tuple to str
-    file_name = file_names.loc[0, 0].replace(" ", "")
-    print("file_name", file_name)
-    xml_path = os.path.join(lbl_str, file_name+".xml")
-    print("xml_path", xml_path)
-
-    tree = ET.parse(xml_path) 
-    xml_root = tree.getroot() 
-
-
-
-    # ------------
-    tensor = torch.zeros(640, 512)
-    # tensor[:,1:50] = 1
-    # print(tensor)
-
-    # ------------
-
-    new_tensor = get_tensor_painted(xml_root, tensor)
-    new_tensor = torch.transpose(new_tensor, 0,1)
-    image = tensor_to_image(new_tensor)
-
-    im1 = image.save("labels_ss/imageasdf5.jpg")
 
 def get_images():
     # define folders and file
@@ -126,8 +61,7 @@ def get_images():
 
     # get path of the xml file
     file_names = pd.read_csv(txt_file, sep=" ", header=None)
-    lbl_str = ''.join(label_path) #change tuple to str
-    # file_name = file_names.loc[0, 0].replace(" ", "")
+    lbl_str = ''.join(label_path)  #change tuple to str
 
     for index, row in file_names.iterrows():
         file_name = file_names.loc[index, 0].replace(" ", "")
@@ -150,37 +84,15 @@ def get_images():
         new_tensor = torch.transpose(new_tensor, 0,1)
         image = tensor_to_image(new_tensor)
 
-
-
         image.save("labels_ss/"+file_id)
 
-        # if index == 0:
-        #    break
+        if index == 0:
+            break
 
 
 def main():
     get_images()
-
-
     print("yay")
-    
-    '''
-    print(root)
-    print(root[0].attrib) 
-    print(root[2][0].text) 
-    print(root[5][0].text)  # <- first object
-    #print(tree.shape)
-    print(sys.getsizeof(tree))
-    length = len(root.findall('object'))
-    print(length)
-    '''
-
-
-
-
-
-    
-
 
 
 if __name__ == "__main__":
